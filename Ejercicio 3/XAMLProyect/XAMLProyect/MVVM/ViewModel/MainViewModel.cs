475d795b2a666a89efd72d321c9c9245ad592f88
UO271572 <91533653+UO271572@users.noreply.github.com>
2022-11-08 20:46:05 +0100

    Add files via upload

---
+++ Ejercicio 3/XAMLProyect/XAMLProyect/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XAMLProyect.Core;
+
+namespace XAMLProyect.MVVM.ViewModel
+{
+    class MainViewModel:ObservableObject
+    {
+
+        private object _currentView;
+        public object CurrentView { 
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+                    }
+        }
+        public HomeViewModel HomeView { set; get; }
+
+        public MainViewModel()
+        {
+            HomeView = new HomeViewModel();
+            _currentView = HomeView;
+        }
+    }
+}
